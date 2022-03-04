a8e96aae0d50aa5d3dd887ae141dba5769d3acee
andresilvatelles <andresilvatelles@gmail.com>
2022-03-04 11:47:14 -0300

    creating CRUD operations for project table

--- lib/src/data/sql_handler.dart
+++ lib/src/data/sql_handler.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 
+import 'project_model.dart';
+
 class DBProvider {
   DBProvider._();
   static final DBProvider db = DBProvider._();
@@ -51,4 +53,61 @@ class DBProvider {
       },
     );
   }
+
+  //CRUD operations for Project table
+  //CREATE
+  Future<int> newProject(Project project, {bool useLargestId = true}) async {
+    final db = await database;
+    if (useLargestId) {
+      var table = await db.rawQuery("SELECT MAX(id)+1 as id FROM  Project");
+      int id = table.first["id"] as int;
+      var raw = await db.rawInsert(
+          "INSERT Into Project (id, name, color)"
+          " VALUES (?,?,?)",
+          [id, project.name, project.color]);
+      return raw;
+    } else {
+      var raw = await db.rawInsert(
+          "INSERT Into Project (id, name, color)"
+          " VALUES (?,?,?)",
+          [project.id, project.name, project.color]);
+      return raw;
+    }
+  }
+
+  //READ
+  Future<Project?> getProjectById(int id) async {
+    final db = await database;
+    var result = await db.query("Project", where: "id = ?", whereArgs: [id]);
+    return result.isNotEmpty ? Project.fromMap(result.first) : null;
+  }
+
+  Future<List<Project>?> getAllProjects() async {
+    final db = await database;
+    var result = await db.query("Projects");
+    List<Project>? list =
+        result.isNotEmpty ? result.map((e) => Project.fromMap(e)).toList() : [];
+    return list;
+  }
+
+  //UPDATE
+  Future<int> updateProject(Project newProject) async {
+    final db = await database;
+    var res = db.update("Project", newProject.toMap(),
+        where: "id = ?", whereArgs: [newProject.id]);
+    return res;
+  }
+
+  //DELETE
+  Future<void> deleteProject(int id) async {
+    final db = await database;
+    db.delete("Project", where: "id = ?", whereArgs: [id]);
+  }
+
+  /*
+  Future<void> deleteAllProjects() async{
+    final db = await database;
+    db.rawDelete("DELETE * From Project");
+  }
+  */
 }
